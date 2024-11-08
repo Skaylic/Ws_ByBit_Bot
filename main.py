36008b17d713add1de8cb024302d942a3523a925
Igor Konovalenko <skaylic@yandex.ru>
2024-11-08 12:10:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
-import asyncio
 import os
-
 from dotenv import load_dotenv
-from time import sleep
-from websockets import ConnectionClosedError
 from skay.Logger import setup_logger
 from skay.Bot import Bot
 
-
 load_dotenv()
 
-logger = setup_logger(os.getenv("BOT_NAME"))
+logger = setup_logger("SkayBot")
 
 bot = Bot()
 
 
-def main():
+if __name__ == '__main__':
     try:
-        asyncio.run(bot.run())
+        bot.start()
     except KeyboardInterrupt:
-        logger.info('KeyboardInterrupt')
-    except ConnectionClosedError:
-        logger.error("ConnectionClosedError")
-        sleep(60)
-        main()
-    except TimeoutError:
-        logger.error("ConnectionClosedError")
-        sleep(60)
-        main()
-
-
-if __name__ == "__main__":
-    main()
+        logger.info("KeyboardInterrupt")
+    except Exception as e:
+       print(e)
