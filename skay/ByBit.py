252a2aef79966cee63c585435132ed8335200034
Igor Konovalenko <skaylic@yandex.ru>
2024-11-08 12:12:37 +0900

    Update ByBit.py

--- skay/ByBit.py
+++ skay/ByBit.py
@@ -1,121 +1,95 @@
-import logging
 import os
-from dotenv import load_dotenv
-import hmac
-import json
-import time
-import websockets
-from pprint import pp
-from pybit.unified_trading import HTTP
+from pybit.unified_trading import WebSocket, HTTP
+import logging
 
-load_dotenv()
+logger = logging.getLogger("SkayBot")
 
 
-class ByBit:
+class ByBit():
 
     def __init__(self):
-        self.logger = logging.getLogger(os.getenv('BOT_NAME'))
-        self.logger.info("Bot is started!")
-        self.api_key = os.getenv("API_KEY")
-        self.api_secret = os.getenv("API_SECRET")
-        self.session = HTTP(
+        logger.info("Bot is loaded!")
+        self.api_key = os.getenv("API_KEY", 0)
+        self.api_secret = os.getenv("API_SECRET", 0)
+        self.interval = os.getenv("INTERVAL", 0)
+        self.symbol = os.getenv("SYMBOL", "")
+        self.qty = float(os.getenv("QTY", 0.0))
+        self.instruments = {}
+        self.quoteCoin = ""
+        self.baseCoin = ""
+        self.min_qty = 0.0
+        self.kline = {}
+        self.balance = {}
+        self.orderId = False
+        self.order = False
+
+    def callback_public(self, message):
+        if message.get("success") is True:
+            logger.info("Kline chanel connection!")
+        data = message.get("data", {})
+        if data:
+            self.kline = {
+                "open": float(data[0]["open"]),
+                "close": float(data[0]["close"])
+            }
+
+    def callback_wallet(self, message):
+        if message.get("success") is True and message.get("req_id"):
+            logger.info("Wallet chanel connection!")
+        data = message.get("data", {})
+        if data:
+            for i in data["coin"]:
+                if i["coin"] == self.quoteCoin:
+                    self.balance[self.quoteCoin] = i["walletBalance"]
+                elif i['coin'] == self.baseCoin:
+                    self.balance[self.baseCoin] = i["walletBalance"]
+
+    def callback_order(self, message):
+        if message.get("success") is True and message.get("req_id"):
+            logger.info("Order chanel connection!")
+        data = message.get("data", {})
+        if data:
+            self.order = data[0]
+            print(self.order)
+
+    def getKline(self):
+        ws = WebSocket(
+            testnet=False,
+            channel_type="spot",
+            # trace_logging=True,
+            callback_function=self.callback_public
+        )
+        ws.kline_stream(self.interval, self.symbol, self.callback_public)
+
+    def getWallet(self):
+        ws = WebSocket(
+            testnet=False,
+            channel_type="private",
+            api_key=os.getenv("API_KEY", 0),
+            api_secret=os.getenv("API_SECRET", 0),
+            # trace_logging=True,
+            callback_function=self.callback_wallet
+        )
+        ws.wallet_stream(self.callback_wallet)
+
+    def getOrder(self):
+        ws = WebSocket(
+            testnet=False,
+            channel_type="private",
+            api_key=os.getenv("API_KEY", 0),
+            api_secret=os.getenv("API_SECRET", 0),
+            # trace_logging=True,
+            callback_function=self.callback_order
+        )
+        ws.order_stream(self.callback_order)
+
+    def getInstruments(self):
+        session = HTTP(
             testnet=False,
             api_key=self.api_key,
             api_secret=self.api_secret,
         )
-        self.symbol = os.getenv("SYMBOL")
-        self.interval = os.getenv("INTERVAL")
-        self.instruments = None
-        self.min_qty = 0.0
-        self.baseCoin = ''
-        self.quoteCoin = ''
-        self.balance = {}
-        self.kline = {}
-        self.orderId = None
-        self.order = None
-
-    async def send(self, ws, op: str, args: list, ids=''):
-        if not ids:
-            subs = dict(op=op, args=args)
-        else:
-            subs = dict(id=ids, op=op, args=args)
-        await ws.send(json.dumps(subs))
-
-    def sign(self):
-        expires = int((time.time() + 10) * 1000)
-        _val = f'GET/realtime{expires}'
-        signature = str(hmac.new(
-            bytes(self.api_secret, 'utf-8'),
-            bytes(_val, 'utf-8'), digestmod='sha256'
-        ).hexdigest())
-        return expires, signature
-
-    async def callbackMessage(self, ws):
-        while True:
-            try:
-                msg = json.loads(await ws.recv())
-                op = msg.get('op', None)
-                type = msg.get('type', None)
-                topic = msg.get('topic', None)
-                data = msg.get('data', None)
-            except websockets.ConnectionClosedOK:
-                print('Connection closed')
-                break
-            if op == 'auth' and msg['success'] == True:
-                return "login"
-            if topic and msg['topic'] == f"kline.{self.interval}.{self.symbol}":
-                self.kline = msg['data'][0]
-            elif topic and msg['topic'] == 'order':
-                self.order = msg['data'][0]
-            elif topic and msg['topic'] == 'wallet':
-                for i in data[0]['coin']:
-                    if i['coin'] == self.quoteCoin:
-                        self.balance[self.quoteCoin] = float(i['walletBalance'])
-                    elif i['coin'] == self.baseCoin:
-                        self.balance[self.baseCoin] = float(i['walletBalance'])
-            elif msg['success']:
-                continue
-            else:
-                pp(msg)
-
-    async def ws_private(self):
-        url = 'wss://stream.bybit.com/v5/private'
-
-        async with websockets.connect(url) as self.ws:
-            e, s = self.sign()
-            await self.send(self.ws, 'auth', [self.api_key, e, s])
-            r = await self.callbackMessage(self.ws)
-            if r == 'login':
-                await self.send(self.ws, 'subscribe', ["wallet"])
-                await self.send(self.ws, 'subscribe', ['order'])
-                await self.callbackMessage(self.ws)
-
-    async def ws_public(self):
-        url = 'wss://stream.bybit.com/v5/public/spot'
-        async with websockets.connect(url) as self.ws_1:
-            await self.send(self.ws_1, 'subscribe', [f"kline.{self.interval}.{self.symbol}"])
-            await self.callbackMessage(self.ws_1)
-
-    def sendTicker(self, qty, side="Buy", tag=''):
-        marketUnit = "baseCoin"
-        if side == "Sell":
-            marketUnit = "quoteCoin"
-        r = self.session.place_order(
-            category="spot",
-            symbol=self.symbol,
-            side=side,
-            orderType="Market",
-            qty=qty,
-            price=self.kline['close'],
-            marketUnit=marketUnit,
-            orderLinkId=tag
-        )
-        self.orderId = r['result']['orderId']
-        self.getOrderHistory(self.orderId)
-        return self
-
-    def getInstruments(self):
-        r = self.session.get_instruments_info(
+        r = session.get_instruments_info(
             category="spot",
             symbol=self.symbol,
         )
@@ -127,7 +101,12 @@ class ByBit:
         return self
 
     def getBalance(self):
-        r = self.session.get_wallet_balance(
+        session = HTTP(
+            testnet=False,
+            api_key=self.api_key,
+            api_secret=self.api_secret,
+        )
+        r = session.get_wallet_balance(
             accountType="UNIFIED",
         )
         for i in r['result']['list'][0]['coin']:
@@ -136,18 +115,24 @@ class ByBit:
             elif i['coin'] == self.baseCoin:
                 self.balance[self.baseCoin] = float(i['walletBalance'])
 
-    def getOrderHistory(self, ordId=''):
-        if not ordId:
-            r = self.session.get_order_history(
-                category="spot",
-                simbol=self.symbol,
-                orderId=ordId,
-            )
-        else:
-            r = self.session.get_order_history(
-                category="spot",
-                simbol=self.symbol,
-                limit=50
-            )
-        data = r['result']['list']
-        return data
+    def sendTicker(self, sz, side="Buy", tag=""):
+        coin = "quoteCoin"
+        if side == "Sell":
+            coin = "baseCoin"
+        session = HTTP(
+            testnet=False,
+            api_key=self.api_key,
+            api_secret=self.api_secret,
+        )
+        r = session.place_order(
+            category="spot",
+            symbol=self.symbol,
+            side=side,
+            orderType="Market",
+            qty=sz,
+            price=self.kline['close'],
+            marketUnit=coin,
+            orderLinkId=tag
+        )
+        self.orderId = r['result']['orderId']
+
